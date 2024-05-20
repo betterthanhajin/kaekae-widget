6cba3421c71b8f6a734a829bc0eb72b477c5d2f1
Hajin Lee <betterthanhajin@icloud.com>
2024-05-20 21:53:18 +0900

    feat:초기설정

---
+++ forge.config.js
@@ -0,0 +1,67 @@
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
+
+module.exports = {
+  packagerConfig: {
+    osxSign: {},
+    osxNotarize: {
+      tool: "notarytool",
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
+    },
+    asar: true,
+  },
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "betterthanhajin",
+          name: "kaekae-widget",
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+      config: {
+        oxSign: false,
+      },
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: "@electron-forge/plugin-auto-unpack-natives",
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const { app, BrowserWindow } = require("electron");
-
+const { app, BrowserWindow, ipcMain } = require("electron/main");
+const path = require("node:path");
+// const updateApp = require("update-electron-app")();
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
-
   win.loadFile("index.html");
 };
-
 app.whenReady().then(() => {
+  ipcMain.handle("ping", () => "pong");
   createWindow();
 });
