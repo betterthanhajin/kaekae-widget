3aba6c30cd3f85e38a3bcc02c048604d7a28acfc
Hajin Lee <betterthanhajin@icloud.com>
2024-05-27 01:33:09 +0900

    feat:kaeake-widget publish

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require("electron/main");
-const { download } = require("electron-dl");
+
 const path = require("node:path");
 // const updateApp = require("update-electron-app")();
 const createWindow = () => {
@@ -12,12 +12,6 @@ const createWindow = () => {
       preload: path.join(__dirname, "preload.js"),
     },
   });
-  // window.loadURL(`file://${__dirname}/index.html`);
-  // ipcMain.on("download", (event, info) => {
-  //   download(BrowserWindow.getFocusedWindow(), info.url, info.properties).then(
-  //     (dl) => window.webContents.send("download complete", dl.getSavePath())
-  //   );
-  // });
   win.setAlwaysOnTop(true, "screen");
   win.loadFile("index.html");
 };
