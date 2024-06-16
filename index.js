f34a5b4db6f1d762ba1e706c9d03c8874a031247
Hajin Lee <betterthanhajin@icloud.com>
2024-06-17 00:19:28 +0900

    feat: 컨텐츠 변경

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("node:path");
 // const updateApp = require("update-electron-app")();
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
+    width: 1000,
     height: 600,
     transparent: true,
     frame: false,
